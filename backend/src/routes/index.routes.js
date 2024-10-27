beba06afe1f3251b64eeee6014da25f1c6ae49a3
HikkizZ <fmiranda.reb@gmail.com>
2024-10-27 19:51:26 -0300

    Se agregan las rutas de calificaciones

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 // Importing routes
 import userRoutes from './user.routes.js';
 import authRoutes from './auth.routes.js';
+import gradeRoutes from './grade.routes.js';
 
 const router = Router(); //? It creates a new instance of the express router.
 
 router
     .use('/user', userRoutes) //! http://localhost:3000/api/user
-    .use('/auth', authRoutes); //! http://localhost:3000/api/auth
+    .use('/auth', authRoutes) //! http://localhost:3000/api/auth
+    .use('/grade', gradeRoutes); //! http://localhost:3000/api/grade
 
 // Exporting the routers
 export default router;
