2ddd9d79b1765c7c08889b9689739c196be2900c
HikkizZ <fmiranda.reb@gmail.com>
2024-10-28 13:34:13 -0300

    MOD index.routes.js
    Se agregran las rutas para asignaturas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,13 +4,15 @@ import { Router } from "express";
 import userRoutes from './user.routes.js';
 import authRoutes from './auth.routes.js';
 import gradeRoutes from './grade.routes.js';
+import subjectRoutes from './subject.routes.js';
 
 const router = Router(); //? It creates a new instance of the express router.
 
 router
     .use('/user', userRoutes) //! http://localhost:3000/api/user
     .use('/auth', authRoutes) //! http://localhost:3000/api/auth
-    .use('/grade', gradeRoutes); //! http://localhost:3000/api/grade
+    .use('/grade', gradeRoutes) //! http://localhost:3000/api/grade
+    .use('/subject', subjectRoutes); //! http://localhost:3000/api/subject
 
 // Exporting the routers
 export default router;
