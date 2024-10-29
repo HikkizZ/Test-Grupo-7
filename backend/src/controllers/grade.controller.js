b42dc287a660e43aec5c2f322eb2f289647c364f
HikkizZ <fmiranda.reb@gmail.com>
2024-10-29 20:10:57 -0300

    MOD grade.controller.js
    Se actualizan controladores de notas

--- backend/src/controllers/grade.controller.js
+++ backend/src/controllers/grade.controller.js
@@ -1,8 +1,12 @@
 "use strict";
 
 import {
-    addGrade
-} from "../services/grades.service.js";
+    createGradeService,
+    getGradeService,
+    getGradesService,
+    updateGradeService,
+    deleteGradeService
+} from "../services/grade.service.js";
 
 import {
     handleSuccess,
@@ -10,30 +14,92 @@ import {
     handleErrorServer
 } from "../handlers/responseHandlers.js";
 
-export async function createGrade(req, res) {
+import {
+    gradeQueryValidation,
+    gradeBodyValidation
+} from "../validations/grade.validation.js";
+
+export async function createGrade(req, res) { //* This function creates a grade.
     try {
-        const { studentId, subject, grade, weight } = req.body;
-        const teacherId = req.user.id; // Asumimos que el usuario autenticado es el profesor
-        const newGrade = await addGrade(studentId, teacherId, subject, grade, weight);
-        handleSuccess(res, 201, "Grade created successfully", newGrade);
+        const { grade, AlumnoId, SubjectId } = req.body;
+
+        const { error } = gradeBodyValidation.validate({ grade, AlumnoId, SubjectId }); //? Validating the body parameters.
+
+        if (error) return handleErrorClient(res, 400, error.message); //? If the body parameters are invalid, return a 400 error.
+
+        const [gradeCreated, errorGrade] = await createGradeService({ grade, AlumnoId, SubjectId }); //? Creating the grade.
+
+        if (errorGrade) return handleErrorClient(res, 400, errorGrade); //? If an error occurred while creating the grade, return a 400 error.
+
+        handleSuccess(res, 201, "Grade created", gradeCreated); //? If the grade is created, return the grade.
     } catch (error) {
-        handleErrorServer(res, 500, "Internal server error.", error.message);
+        handleErrorServer(res, 500, "Internal server error", error.message); //? If an error occurred, return a 500 error.
+    }
+};
+
+export async function getGrade(req, res) { //* This function gets a grade by id.
+    try {
+        const { id } = req.query;
+
+        const { error } = gradeQueryValidation.validate({ id }); //? Validating the query parameters.
+
+        if (error) return handleErrorClient(res, 400, error.message); //? If the query parameters are invalid, return a 400 error.
+
+        const [gradeFound, errorGrade] = await getGradeService({ idGrade: id }); //? Getting the grade.
+
+        if (errorGrade) return handleErrorClient(res, 400, errorGrade); //? If an error occurred while getting the grade, return a 400 error.
+
+        handleSuccess(res, 200, "Grade found", gradeFound); //? If the grade is found, return the grade.
+    } catch (error) {
+        handleErrorServer(res, 500, "Internal server error", error.message); //? If an error occurred, return a 500 error.
+    }
+};
+
+export async function getGrades(req, res) { //* This function gets all the grades.
+    try {
+        const [grades, errorGrades] = await getGradesService(); //? Getting the grades.
+
+        if (errorGrades) return handleErrorClient(res, 400, errorGrades); //? If an error occurred while getting the grades, return a 400 error.
+
+        handleSuccess(res, 200, "Grades found", grades); //? If the grades are found, return the grades.
+    } catch (error) {
+        handleErrorServer(res, 500, "Internal server error", error.message); //? If an error occurred, return a 500 error.
+    }
+};
+
+export async function updateGrade(req, res) { //* This function updates a grade by id.
+    try {
+        const { id } = req.query;
+        const data = req.body;
+
+        const { error } = gradeQueryValidation.validate({ id }); //? Validating the query parameters.
+
+        if (error) return handleErrorClient(res, 400, error.message); //? If the query parameters are invalid, return a 400 error.
+
+        const [gradeUpdated, errorGrade] = await updateGradeService(id, data); //? Updating the grade.
+
+        if (errorGrade) return handleErrorClient(res, 400, errorGrade); //? If an error occurred while updating the grade, return a 400 error.
+
+        handleSuccess(res, 200, "Grade updated", gradeUpdated); //? If the grade is updated, return the grade.
+    } catch (error) {
+        handleErrorServer(res, 500, "Internal server error", error.message); //? If an error occurred, return a 500 error.
     }
 }
 
-export async function getGradesByStudent(req, res) {
+export async function deleteGrade(req, res) { //* This function deletes a grade by id.
     try {
-        const { studentId, subjectId } = req.params;
+        const { id } = req.query;
 
-        const grades = await gradeRepository.find({
-            where: { studentId, subjectId }
-        });
+        const { error } = gradeQueryValidation.validate({ id }); //? Validating the query parameters.
 
-        const total = grades.reduce((sum, grade) => sum + grade.grade, 0);
-        const averageSubject = grades.length ? total / grades.length : 0;
+        if (error) return handleErrorClient(res, 400, error.message); //? If the query parameters are invalid, return a 400 error.
 
-        handleSuccess(res, 200, "Grades found successfully", { grades, averageSubject });
+        const [gradeDeleted, errorGrade] = await deleteGradeService({ idGrade: id }); //? Deleting the grade.
+
+        if (errorGrade) return handleErrorClient(res, 400, errorGrade); //? If an error occurred while deleting the grade, return a 400 error.
+
+        handleSuccess(res, 200, "Grade deleted", gradeDeleted); //? If the grade is deleted, return the grade.
     } catch (error) {
-        handleErrorServer(res, 500, "Internal server error.", error.message);
+        handleErrorServer(res, 500, "Internal server error", error.message); //? If an error occurred, return a 500 error.
     }
-}
+};
