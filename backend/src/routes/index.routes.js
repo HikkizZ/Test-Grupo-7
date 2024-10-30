fa0bcc5737af1818bd59b75d3a41703471a4fad6
HallamSaavedra <hallam.saavedra2001@alumnos.ubiobio.cl>
2024-10-30 19:46:03 -0300

    avanze en el crud

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 // Importing routes
+import actividadRoutes from './actividad.routes.js';
 import userRoutes from './user.routes.js';
 import authRoutes from './auth.routes.js';
 
 const router = Router(); //? It creates a new instance of the express router.
 
 router
+    .use('/actividad', actividadRoutes)
     .use('/user', userRoutes) //! http://localhost:3000/api/user
     .use('/auth', authRoutes); //! http://localhost:3000/api/auth
 
