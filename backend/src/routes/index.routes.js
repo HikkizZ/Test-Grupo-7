94a9ff8995ed82828222081f0d952a7021e35ddf
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-10-30 14:54:22 -0300

    Se comenta rutas no utilizadas por problemas no solucionados

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,7 +4,7 @@ import { Router } from "express";
 import userRoutes from './user.routes.js';
 import authRoutes from './auth.routes.js';
 import Room from './room.routes.js';
-import Reservation from './reservation.routes.js';
+//import Reservation from './reservation.routes.js';
 import Resource from './resource.routes.js';
 
 const router = Router(); //? It creates a new instance of the express router.
@@ -13,7 +13,7 @@ router
     .use('/user', userRoutes) //! http://localhost:3000/api/user
     .use('/auth', authRoutes) //! http://localhost:3000/api/auth
     .use('/room', Room) //! http://localhost:3000/api/room
-    .use('/reservation', Reservation) //! http://localhost:3000/api/reservation
+//    .use('/reservation', Reservation) //! http://localhost:3000/api/reservation
     .use('/resource', Resource); //! http://localhost:3000/api/resource
 
 // Exporting the routers
