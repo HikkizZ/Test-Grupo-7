381e71e6c835969cbc4a0934bc1062298595b6a6
Patricia Yulihana González Caamaño <165500389+Pygc@users.noreply.github.com>
2024-10-30 03:09:31 -0300

    MOD index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,18 +3,18 @@ import { Router } from "express";
 // Importing routes
 import userRoutes from './user.routes.js';
 import authRoutes from './auth.routes.js';
-import gradeRoutes from './grade.routes.js';
-import subjectRoutes from './subject.routes.js';
-import cursoRoutes from './curso.routes.js';
+import Room from './room.routes.js';
+import Reservation from './reservation.routes.js';
+import Resource from './resource.routes.js';
 
 const router = Router(); //? It creates a new instance of the express router.
 
 router
     .use('/user', userRoutes) //! http://localhost:3000/api/user
     .use('/auth', authRoutes) //! http://localhost:3000/api/auth
-    .use('/grade', gradeRoutes) //! http://localhost:3000/api/grade
-    .use('/subject', subjectRoutes) //! http://localhost:3000/api/subject
-    .use('/curso', cursoRoutes); //! http://localhost:3000/api/curso
+    .use('/room', Room) //! http://localhost:3000/api/room
+    .use('/reservation', Reservation) //! http://localhost:3000/api/reservation
+    .use('/resource', Resource); //! http://localhost:3000/api/resource
 
 // Exporting the routers
 export default router;
