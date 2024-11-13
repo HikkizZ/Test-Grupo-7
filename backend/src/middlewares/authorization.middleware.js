7ddac38db67cf8d72826dee23e8eafe298a14b44
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-11-13 14:10:07 -0300

    se actualiza verifyRole

--- backend/src/middlewares/authorization.middleware.js
+++ backend/src/middlewares/authorization.middleware.js
@@ -31,11 +31,17 @@ export async function isAdmin(req, res, next) { //? Function that checks if the 
     }
 }
 
-export function verifyRole(requiredRole) {
+export function verifyRole(requiredRoles) {
     return (req, res, next) => {
         const user = req.user; //* Se asume que el usuario ya está autenticado y disponible en req.user
         
-        if (!user || user.role !== requiredRole) {
+        if (!user) {
+            return res.status(403).json({ message: "No tienes permisos para acceder a esta ruta." });
+        }
+
+        const roles = Array.isArray(requiredRoles) ? requiredRoles : [requiredRoles];
+        
+        if (!roles.includes(user.role)) {
             return res.status(403).json({ message: "No tienes permisos para acceder a esta ruta." });
         }
         
