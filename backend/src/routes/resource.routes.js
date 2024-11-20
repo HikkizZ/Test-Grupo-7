2910416759c60b4178177ca92abf26d8b5b1e665
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-11-20 20:31:41 -0300

    actualizo rutas de recurso y sala

--- backend/src/routes/resource.routes.js
+++ backend/src/routes/resource.routes.js
@@ -11,8 +11,8 @@ router
 router
     .post("/", verifyRole("admin"), createResource) // Crear un recurso
     .get("/all", getResources) // Listar recursos (disponibles y no disponibles)
-    .patch("/detail/", verifyRole(["Encargado", "admin"]), updateResource) // Actualizar recurso
-    .get("/detail/", getResource) // Mostrar información de un recurso en particular
-    .delete("/detail/", verifyRole("admin"), deleteResource); // Eliminar un recurso
+    .patch("/update/", verifyRole(["Encargado", "admin"]), updateResource) // Actualizar recurso
+    .get("/get/", getResource) // Mostrar información de un recurso en particular
+    .delete("/delete/", verifyRole("admin"), deleteResource); // Eliminar un recurso
 
 export default router;
--- backend/src/routes/room.routes.js
+++ backend/src/routes/room.routes.js
@@ -11,8 +11,8 @@ router
 router
     .post("/", verifyRole("admin"), createRoom) // Crear una aula
     .get("/all", getRooms) // Listar aulas (disponibles y no disponibles)
-    .patch("/detail/", verifyRole(["Encargado", "admin"]), updateRoom) // Actualizar aula
-    .get("/detail/", getRoom) // Mostrar información de un aula en particular
-    .delete("/detail/", verifyRole("admin"), deleteRoom); // Eliminar un aula
+    .patch("/update/", verifyRole(["Encargado", "admin"]), updateRoom) // Actualizar aula
+    .get("/get/", getRoom) // Mostrar información de un aula en particular
+    .delete("/delete/", verifyRole("admin"), deleteRoom); // Eliminar un aula
 
 export default router;
