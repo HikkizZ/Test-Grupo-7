ba3483dc3daa44ad8c35503e7cac04ebb1fd4be9
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-11-21 23:28:04 -0300

    UPDATE PERIOD.ROUTES.JS

--- backend/src/routes/period.routes.js
+++ backend/src/routes/period.routes.js
@@ -1,13 +1,21 @@
 import { Router } from 'express';
-import { getAllPeriods, getPeriodById, createPeriod, updatePeriod, deletePeriod} from '../controllers/period.controller.js';
+import { authenticateJWT } from '../middlewares/authentication.middleware.js';
+import { isAdmin, verifyRole } from '../middlewares/authorization.middleware.js';
+
+import { getAllPeriods, 
+        getPeriodById, 
+        createPeriod, 
+        updatePeriod, 
+        deletePeriod} 
+from '../controllers/period.controller.js';
 
 const router = Router();
 
 router
-    .get('/', getAllPeriods) //! http://localhost:3000/api/period/
-    .get('/:id', getPeriodById) //! http://localhost:3000/api/period/:id
-    .post('/create', createPeriod) //! http://localhost:3000/api/period/create
-    .put('/update/:id', updatePeriod) //! http://localhost:3000/api/period/update/:id
-    .delete('/delete/:id', deletePeriod); //! http://localhost:3000/api/period/delete/:id
+        .get('/', getAllPeriods)
+        .get('/:id', getPeriodById)           
+        .post('/create', verifyRole('admin'),createPeriod)       
+        .put('/update/:id', verifyRole('admin'),updatePeriod)    
+        .delete('/delete/:id', verifyRole('admin'),deletePeriod);
 
 export default router;
