d4d1f71aa09d960c8dbe0ac6847549e7c04d610c
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-11-25 22:35:38 -0300

    Actualización de las rutas del periodo

--- backend/src/routes/period.routes.js
+++ backend/src/routes/period.routes.js
@@ -1,22 +1,28 @@
-import { Router } from 'express';
-import { authenticateJWT } from '../middlewares/authentication.middleware.js';
-import {  verifyRole } from '../middlewares/authorization.middleware.js';
-import { 
-        getAllPeriods, 
-        getPeriodById, 
-        createPeriod, 
-        updatePeriod, 
-        deletePeriod} 
-from '../controllers/period.controller.js';
+"use strict";
+
+import { Router } from "express";
+import { authenticateJWT } from "../middlewares/authentication.middleware.js";
+import { verifyRole } from "../middlewares/authorization.middleware.js";
+import {
+    createPeriod,
+    getPeriod,
+    getPeriods,
+    updatePeriod,
+    deletePeriod,
+} from "../controllers/period.controller.js";
 
 const router = Router();
+
+// Middleware global para autenticar JWT
+router.use(authenticateJWT);
+
+// Definición de las rutas para la entidad "Period"
 router
-        .use(authenticateJWT);
-router
-        .get('/', getAllPeriods)
-        .get('/:id', getPeriodById)           
-        .post('/create', verifyRole('admin'),createPeriod)       
-        .put('/update/:id', verifyRole('admin'),updatePeriod)    
-        .delete('/delete/:id', verifyRole('admin'),deletePeriod);
+    .get("/", getPeriod) // Obtener un período específico por id o nombre
+    .get("/all", getPeriods) // Obtener todos los períodos
+    .post("/", verifyRole("admin"), createPeriod) // Crear un período (solo administradores)
+    .patch("/", verifyRole("admin"), updatePeriod) // Actualizar un período (solo administradores)
+    .delete("/", verifyRole("admin"), deletePeriod); // Eliminar un período (solo administradores)
+
 
 export default router;
