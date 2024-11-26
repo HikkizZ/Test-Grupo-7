b28341c5e92d0fe8fce984414c991b17d2a2807c
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-11-25 22:26:59 -0300

    Actualización del modelo del periodo

--- backend/src/models/period.model.js
+++ backend/src/models/period.model.js
@@ -23,7 +23,14 @@ const PeriodSchema = new EntitySchema({
             type: "time",
             nullable: true,
         },
-    },
+    },relations:{
+        schedules: { // Relación con Schedule
+            target: "Schedule", 
+            type: "one-to-many", 
+            inverseSide: "subject", 
+            onDelete: "CASCADE", 
+        },
+    }
 });
 
 export default PeriodSchema;
