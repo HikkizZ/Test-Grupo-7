886dd87b05fc516bf8ece27f57750293a6cf623a
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2024-12-03 14:15:38 -0300

    MOD subject.services.js
    Se actualizan controladores. Se elimina busqueda de asignaturas por nombre. Se implementa la verificación del rol 'profesor' al asignar un rut a una asignatura. Se actualiza el update

--- backend/src/services/subject.service.js
+++ backend/src/services/subject.service.js
@@ -1,21 +1,30 @@
 "use strict";
 
-import Subject from "../models/subject.model.js";
 import { AppDataSource } from "../config/configDB.js";
+import Subject from "../models/subject.model.js";
+import Curso from "../models/curso.model.js";
+import User from "../models/user.model.js";
 
 export async function getSubjectService(query) { //* This function gets a subject by id and name.
     try {
-        const { idSubject, nameSubject } = query; //? Getting the id and name of the subject.
+        const { idSubject } = query; //? Getting the id and name of the subject.
 
         const subjectRepository = AppDataSource.getRepository(Subject); //? Getting the subject repository.
 
         const subjectFound = await subjectRepository.findOne({ //? Finding the subject by id and name.
-            where: [{ id: idSubject }, { name: nameSubject }],
+            where: { id: idSubject },
+            relations: ["curso", "teacher"],
         });
 
-        if (!subjectFound) return [null, "Subject not found."]; //? If the subject is not found, return null and a message.
+        if (!subjectFound) return [null, "Asingatura no encontrada"]; //? If the subject is not found, return null and a message.
 
-        return [subjectFound, null];
+        return [{
+            id: subjectFound.id,
+            name: subjectFound.name,
+            description: subjectFound.description,
+            curso: subjectFound.curso.code,
+            teacher: subjectFound.teacher.name,
+        }, null];
     } catch (error) {
         console.error("An error occurred while getting the subject:", error);
         return [null, "Internal server error."];
@@ -30,9 +39,17 @@ export async function getSubjectsService() { //* This function gets all the subj
             relations: ["curso", "teacher"], //? Getting the course and teacher of the subjects.
         });
 
-        if (!subjects || subjects.length === 0) return [null, "Subjects not found."]; //? If the subjects are not found, return null and a message.
+        if (!subjects || subjects.length === 0) return [null, "Asignaturas no encontradas"]; //? If the subjects are not found, return null and a message.
 
-        return [subjects, null];
+        return [{ //? Returning the subjects. Se muestra el id, nombre, descripción, curso y profesor de la asignatura.
+            subjects: subjects.map(subject => ({
+                id: subject.id,
+                name: subject.name,
+                description: subject.description,
+                curso: subject.curso.code,
+                teacher: subject.teacher.name,
+            })),
+        }, null];
     } catch (error) {
         console.error("An error occurred while getting the subjects:", error);
         return [null, "Internal server error."];
@@ -42,16 +59,34 @@ export async function getSubjectsService() { //* This function gets all the subj
 export async function createSubjectService(body) { //* This function creates a subject.
     try {
         const subjectRepository = AppDataSource.getRepository(Subject); //? Getting the subject repository.
+        const cursoRepository = AppDataSource.getRepository(Curso); //? Getting the course repository.
+        const userRepository = AppDataSource.getRepository(User); //? Getting the user repository.
+
+        const { name, description, cursoCode, rutProfesor } = body; //? Getting the body parameters: name, description, cursoCode.
+
+        const cursoFound = await cursoRepository.findOne({ where: { code: cursoCode } }); //? Finding the course by id.
+        if (!cursoFound) return [null, "Curso no encontrado."]; //? If the course is not found, return null and a message.
+
+        const teacherFound = await userRepository.findOne({
+            where: { rut: rutProfesor, role: "profesor"} }); //? Finding the teacher by rut.
+        if (!teacherFound) return [null, "Profesor no encontrado."]; //? If the teacher is not found, return null and a message.
 
         const newSubject = subjectRepository.create({ //? Creating a new subject.
-            name: body.name,
-            description: body.description,
-            curso: {id: body.cursoId}
+            name: name,
+            description: description,
+            curso: cursoFound,
+            teacher: teacherFound,
         });
 
         const subjectCreated = await subjectRepository.save(newSubject); //? Saving the new subject.
 
-        return [subjectCreated, null];
+        return [{
+            id: subjectCreated.id,
+            name: subjectCreated.name,
+            description: subjectCreated.description,
+            curso: subjectCreated.curso.code,
+            teacher: subjectCreated.teacher.name,
+        }, null];
     } catch (error) {
         console.error("An error occurred while creating the subject:", error);
         return [null, "Internal server error."];
@@ -60,19 +95,46 @@ export async function createSubjectService(body) { //* This function creates a s
 
 export async function updateSubjectService(query, body) { //* This function updates a subject by id and name.
     try {
-        const { idSubject, nameSubject } = query; //? Getting the id and name of the subject.
+        const { idSubject } = query; //? Getting the id and name of the subject.
         
         const subjectRepository = AppDataSource.getRepository(Subject); //? Getting the subject repository.
+        const cursoRepository = AppDataSource.getRepository(Curso); //? Getting the course repository.
+        const userRepository = AppDataSource.getRepository(User); //? Getting the user repository.
 
-        const subjectFound = await subjectRepository.findOne({ //? Finding the subject by id and name.
-            where: [{ id: idSubject }, { name: nameSubject }],
+        const subjectFound = await subjectRepository.findOne({ //? Finding the subject by id.
+            where: { id: idSubject },
+            relations: ["curso", "teacher"],
         });
+        if (!subjectFound) return [null, "Asignatura no encontrada"]; //? If the subject is not found, return null and a message.
 
-        if (!subjectFound) return [null, "Subject not found."]; //? If the subject is not found, return null and a message.
+        //° Buscar el curso por código si se proporciona.
+        if (body.cursoCode) {
+            const curso = await cursoRepository.findOne({ where: { code: body.cursoCode } }); //? Se busca el curso por código.
+            if (!curso) return [null, "Curso no encontrado."];
+            subjectFound.curso = curso;
+        }
 
-        const subjectUpdated = await subjectRepository.update(subjectFound.id, body); //? Updating the subject.
+        //° Buscar el profesor por rut si se proporciona.
+        if (body.rutProfesor) {
+            const teacher = await userRepository.findOne({
+                where: { rut: body.rutProfesor, role: "profesor" }}); //? Se busca el profesor por rut.
+            if (!teacher) return [null, "Profesor no encontrado."];
+            subjectFound.teacher = teacher;
+        }
 
-        return [subjectUpdated, null];
+        //° Actualizar los campos básicos si son proporcionados
+        if (body.name) subjectFound.name = body.name;
+        if (body.description) subjectFound.description = body.description;
+
+        const subjectUpdated = await subjectRepository.save(subjectFound); //? Saving the updated subject.
+
+        return [{
+            id: subjectUpdated.id,
+            name: subjectUpdated.name,
+            description: subjectUpdated.description,
+            curso: subjectUpdated.curso.code,
+            teacher: subjectUpdated.teacher?.name || null, //? If the teacher is null, return null.
+        }, null];
     } catch (error) {
         console.error("An error occurred while updating the subject:", error);
         return [null, "Internal server error."];
@@ -81,15 +143,13 @@ export async function updateSubjectService(query, body) { //* This function upda
 
 export async function deleteSubjectService(query) { //* This function deletes a subject by id and name.
     try {
-        const { idSubject, nameSubject } = query; //? Getting the id and name of the subject.
+        const { idSubject } = query; //? Getting the id and name of the subject.
 
         const subjectRepository = AppDataSource.getRepository(Subject); //? Getting the subject repository.
 
-        const subjectFound = await subjectRepository.findOne({ //? Finding the subject by id and name.
-            where: [{ id: idSubject }, { name: nameSubject }],
-        });
+        const subjectFound = await subjectRepository.findOne({ where: { id: idSubject } }); //? Finding the subject by id.
 
-        if (!subjectFound) return [null, "Subject not found."]; //? If the subject is not found, return null and a message.
+        if (!subjectFound) return [null, "Asignatura no encontrada"]; //? If the subject is not found, return null and a message.
 
         const subjectDeleted = await subjectRepository.delete(subjectFound.id); //? Deleting the subject.
 
