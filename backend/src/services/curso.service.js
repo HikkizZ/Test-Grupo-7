cd62a63e495454cdf29c40c15ffbda27a7bc1b2e
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2024-12-09 18:57:01 -0300

    MOD curso.service.js
    Se importan entidades de Curso y User ademas de importar 'In' de typeORM. Se realizan cambios en los datos que se retornan al usuario junto a las relaciones al buscar un curso. Se crean dos servicios nuevos, para agregar alumnos a un curso y para relacionar las asignaturas de un curso a los estudiantes incritos

--- backend/src/services/curso.service.js
+++ backend/src/services/curso.service.js
@@ -1,7 +1,9 @@
 "use strict";
 
-import Curso from '../models/curso.model.js';
 import { AppDataSource } from '../config/configDB.js';
+import Curso from '../models/curso.model.js';
+import User from '../models/user.model.js';
+import { In } from 'typeorm';
 
 export async function getCursoService(query) { //* This function gets a course by id, code, and name.
     try {
@@ -15,7 +17,18 @@ export async function getCursoService(query) { //* This function gets a course b
 
         if (!cursoFound) return [null, "No se ha encontrado el curso."]; //? If the course is not found, return null and a message.
 
-        return [cursoFound, null];
+        return [{
+            cursoFound: { //? Returning the course. Se muestra el id, nombre, nivel, año, sección y código del curso.
+                name: cursoFound.name,
+                code: cursoFound.code,
+                subjects: cursoFound.subjects,
+                students: cursoFound.students.map(student => ({ //? Getting the students of the course.
+                    name: student.name,
+                    rut: student.rut,
+                    email: student.email,
+                })),
+            },
+        }, null];
     } catch (error) {
         console.error("Ocurró un error al encontrar el curso:", error);
         return [null, "Error interno del servidor"];
@@ -27,12 +40,23 @@ export async function getCursosService() { //* This function gets all the course
         const cursoRepository = AppDataSource.getRepository(Curso); //? Getting the course repository.
 
         const cursos = await cursoRepository.find({ 
-            relations: ["subjects"], //? Getting the subjects of the courses.
+            relations: ["subjects", "students"], //? Getting the subjects of the courses.
         }); //? Finding all the courses.
 
         if (!cursos || cursos.length === 0) return [null, "Cursos no encontrados."]; //? If the courses are not found, return null and a message.
 
-        return [cursos, null];
+        return [{
+            cursos: cursos.map(curso => ({ //? Returning the courses. Se muestra el id, nombre, nivel, año, sección y código del curso.
+                name: curso.name,
+                code: curso.code,
+                subjects: curso.subjects,
+                students: curso.students.map(student => ({ //? Getting the students of the course.
+                    name: student.name,
+                    rut: student.rut,
+                    email: student.email,
+                })),
+            })),
+        }, null];
     } catch (error) {
         console.error("Ocurrió un error al encontrar los cursos:", error);
         return [null, "Error interno del servidor"];
@@ -99,7 +123,18 @@ export async function updateCursoService(query, body) { //* This function update
         
         const cursoUpdated = await cursoRepository.save({ ...cursoFound, ...body }); //? Updating the course.
 
-        return [cursoUpdated, null];
+        return [{
+            cursoUpdated: { //? Returning the course. Se muestra el id, nombre, nivel, año, sección y código del curso.
+                name: cursoUpdated.name,
+                code: cursoUpdated.code,
+                subjects: cursoUpdated.subjects,
+                students: cursoUpdated.students.map(student => ({ //? Getting the students of the course.
+                    name: student.name,
+                    rut: student.rut,
+                    email: student.email,
+                })),
+            },
+        }, null];
     } catch (error) {
         console.error("Ocurrió un error al actualizar el curso:", error);
         return [null, "Error interno del servidor"];
@@ -126,6 +161,82 @@ export async function deleteCursoService(query) { //* This function deletes a co
         return [null, "Error interno del servidor"];
     }
 };
+
+export async function addStudentsToCursoService(cursoCode, studentsRut) {
+    try {
+        const cursoRepository = AppDataSource.getRepository(Curso); //? Getting the course repository.
+        const userRepository = AppDataSource.getRepository(User); //? Getting the user repository.
+
+        const cursoFound = await cursoRepository.findOne({ where: { code: cursoCode }, relations: ['students'] }); //? Finding the course by code.
+        if (!cursoFound) return [null, "Curso no encontrado"]; //? If the course is not found, return null and a message.
+        console.log("studentsRut:", studentsRut);
+        const students = await userRepository.find({ where: { rut: In(studentsRut), role: 'alumno'} }); //? Finding the students by rut.
+        console.log("students:", students);
+        if (!students || students.length === 0) return [null, "Estudiantes no encontrados"]; //? If the students are not found, return null and a message.
+        
+        students.forEach(student => {
+            student.curso = cursoFound;
+        }); //? Adding the students to the course.
+
+        await userRepository.save(students); //? Saving the students.
+
+        const updateCurso = await cursoRepository.findOne({ where: { code: cursoCode }, relations: ['students'] }); //? Finding the course by code.
+
+        return [updateCurso, null];
+    } catch (error) {
+        console.error("Ocurrió un error al agregar los estudiantes al curso:", error);
+        return [null, "Error interno del servidor"];
+    }
+};
+
+export async function assingSubjectsToStudentsService(query) {
+    try {
+        const cursoRepository = AppDataSource.getRepository(Curso); //? Getting the course repository.
+
+        const { cursoCode } = query; //? Getting the query parameters: cursocode.
+
+        const cursoFound = await cursoRepository.findOne({
+            where: { code: cursoCode }, relations: ['students', 'subjects']
+        }); //? Finding the course by code.
+
+        if (!cursoFound) return [null, "Curso no encontrado"]; //? If the course is not found, return null and a message.
+
+        const { students, subjects } = cursoFound; //? Getting the students and subjects of the course.
+
+        console.log("students:", students);
+        console.log("subjects:", subjects);
+
+        const studentSubjects = students.flatMap(student => {
+            return subjects.map(subject => ({
+                student_rut: student.rut,
+                subject_id: subject.id,
+            }));
+        }); //? Getting the subjects of the students.
+
+        await AppDataSource
+            .createQueryBuilder()
+            .insert()
+            .into('student_subjects')
+            .values(studentSubjects)
+            .orIgnore()
+            .execute(); //? Inserting the subjects of the students.
+
+        return [{
+            students: students.map(student => ({ //? Returning the students. Se muestra el nombre, rut y email del estudiante.
+                name: student.name,
+                rut: student.rut,
+                email: student.email,
+                subjects: subjects.map(subject => ({ //? Getting the subjects of the students.
+                    id: subject.id,
+                    name: subject.name
+                })),
+            })),
+        }, null];
+    } catch (error) {
+        console.error("Ocurrió un error al asignar las asignaturas a los estudiantes:", error);
+        return [null, "Error interno del servidor"];
+    }
+}
 
 function generatedSubjectCode(year, level, section){ //* This function generates the course code.
     const yearcode = String(year).slice(-2); //? Getting the last two digits of the year.
