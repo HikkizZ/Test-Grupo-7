28e8f5541e77411e4bb155b3690f808a9ca10821
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-12-12 22:02:42 -0300

    Update Home
    Se añadió un botón que me lleva a Horario

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,25 @@
-const Home = () => {
-  return (
-    <>
-    </>
-  )
-}
+import { useNavigate } from "react-router-dom";
 
-export default Home
+const Home = () => {
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate("/horario");
+    };
+
+    return (
+        <>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+            <button onClick={handleRedirect}>Horarios</button>
+        </>
+    );
+};
+
+export default Home;
