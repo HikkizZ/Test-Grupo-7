427232e389b657450616907e294a8e3cf5f7e4da
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-12-17 11:38:51 -0300

    arreglo el protectedRoute y defino muestras rutas crud especificas segun el rol
    ahora el protectedRoute funciona y implemente que solo el admin puede modificar y eliminar las reservaciones, el encargado puede modificar, y los alumnos y profesores pueden crear reservacion
    
    (todos pueden ver la lista, y el admin tiene todos los permisos)
    
    !me falta decidir/hacer si el encargado debe poder crear reservaciones (por ahora le aparece el boton pero en las rutas del backend no lo agregue para que pueda crear)

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,25 +1,45 @@
-import { createContext, useContext, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({ children }) {
     const navigate = useNavigate();
-    const user = JSON.parse(sessionStorage.getItem('usuario')) || '';
-    const isAuthenticated = user ? true : false;
+    const [user, setUser] = useState(() => {
+        const storedUser = sessionStorage.getItem('usuario');
+        return storedUser ? JSON.parse(storedUser) : null;
+    });
+    const [isAuthenticated, setIsAuthenticated] = useState(!!user);
 
-useEffect(() => {
-    if (!isAuthenticated) {
-        navigate('/auth');
-    }
-}, [isAuthenticated, navigate]);
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/auth'); // Redirige a login si no está autenticado
+        }
+    }, [isAuthenticated, navigate]);
 
-return (
-    <AuthContext.Provider value={{ isAuthenticated, user }}>
-        {children}
-    </AuthContext.Provider>
-);
+    const loginUser = (userData) => {
+        // Guarda la información del usuario en el sessionStorage
+        const userWithRole = {
+            ...userData,
+            role: userData.role, // Usa "role" en lugar de "rol"
+        };
+        sessionStorage.setItem('usuario', JSON.stringify(userWithRole));
+        setUser(userWithRole);
+        setIsAuthenticated(true);
+    };
+
+    const logoutUser = () => {
+        // Elimina el usuario del sessionStorage y actualiza el estado
+        sessionStorage.removeItem('usuario');
+        setUser(null);
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, user, loginUser, logoutUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
