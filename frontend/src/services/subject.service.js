d5a723c9146de3137475809e5ca0912670f14380
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2024-12-17 12:17:24 -0300

    MOD subject.service.js
    Se corrigen errores del servicio de subject

--- frontend/src/services/subject.service.js
+++ frontend/src/services/subject.service.js
@@ -23,6 +23,27 @@ export async function createSubject(data) {
     const response = await axios.post("/subject", data); //* Verifica que esta sea la ruta correcta
     return response.data;
   } catch (error) {
-    return error.response?.data || error.message;
+    return error.response?.data?.message || error.message;
+  }
+}
+
+export async function getSubject(id) {
+  try {
+    const { data } = await axios.get(`/subject/?id=${id}`)
+    return data.data;
+  } catch (error) {
+    console.error("Error al obtener los cursos: ", error.response?.data?.message || error)
+    return error.response?.data?.message || "Error desconocido al obtener la asignatura"
+  }
+}
+
+export async function updateSubject(id, data) {
+  try {
+    const response = await axios.patch(`/subject/?id=${id}`, data); //* Verifica que esta sea la ruta correcta
+    console.log('response', response?.data?.message)
+    return response.data;
+  } catch (error) {
+    console.log("Error al actualizar la asignatura: ", error.response?.data?.message || error)
+    return error.response?.data?.message || error.message;
   }
 }
