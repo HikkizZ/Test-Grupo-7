d8d848331d117f174785a48cdc0ed26ffc9e1eb7
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2024-12-17 14:49:24 -0300

    ADD CursoFormPopup.jsx
    Se crea form para el popup de especifica para el curso

---
+++ frontend/src/components/cursos/CursoFormPopup.jsx
@@ -0,0 +1,113 @@
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import HideIcon from '../../assets/HideIcon.svg';
+import ViewIcon from '../../assets/ViewIcon.svg';
+import '@styles/CustomForm.css';
+
+const CursoFormPopup = ({ title, fields, buttonText, onSubmit, footerContent, backgroundColor }) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const toggleNewPasswordVisibility = () => {
+        setShowNewPassword(!showNewPassword);
+    };
+
+    const onFormSubmit = (data) => {
+        onSubmit(data);
+    };
+
+    return (
+        <form
+            className="custom-form"
+            style={{ backgroundColor: backgroundColor }}
+            onSubmit={handleSubmit(onFormSubmit)}
+            autoComplete="off"
+        >
+            <h1 className="custom-form__title">{title}</h1>
+            {fields.map((field, index) => (
+                <div className="custom-form__input-container" key={index}>
+                    {field.label && <label className="custom-form__label" htmlFor={field.name}>{field.label}</label>}
+                    {field.fieldType === 'input' && (
+                        <input
+                            {...register(field.name, {
+                                required: field.required ? 'Este campo es obligatorio' : false,
+                                minLength: field.minLength ? { value: field.minLength, message: `Debe tener al menos ${field.minLength} caracteres` } : false,
+                                maxLength: field.maxLength ? { value: field.maxLength, message: `Debe tener máximo ${field.maxLength} caracteres` } : false,
+                                pattern: field.pattern ? { value: field.pattern, message: field.patternMessage || 'Formato no válido' } : false,
+                                validate: field.validate || {},
+                            })}
+                            className="custom-form__input"
+                            name={field.name}
+                            placeholder={field.placeholder}
+                            type={field.type === 'password' && field.name === 'password' ? (showPassword ? 'text' : 'password') :
+                                field.type === 'password' && field.name === 'newPassword' ? (showNewPassword ? 'text' : 'password') :
+                                field.type}
+                            defaultValue={field.defaultValue || ''}
+                            disabled={field.disabled}
+                            onChange={field.onChange}
+                        />
+                    )}
+                    {field.fieldType === 'textarea' && (
+                        <textarea
+                            {...register(field.name, {
+                                required: field.required ? 'Este campo es obligatorio' : false,
+                                minLength: field.minLength ? { value: field.minLength, message: `Debe tener al menos ${field.minLength} caracteres` } : false,
+                                maxLength: field.maxLength ? { value: field.maxLength, message: `Debe tener máximo ${field.maxLength} caracteres` } : false,
+                                pattern: field.pattern ? { value: field.pattern, message: field.patternMessage || 'Formato no válido' } : false,
+                                validate: field.validate || {},
+                            })}
+                            className="custom-form__textarea"
+                            name={field.name}
+                            placeholder={field.placeholder}
+                            defaultValue={field.defaultValue || ''}
+                            disabled={field.disabled}
+                            onChange={field.onChange}
+                        />
+                    )}
+                    {field.fieldType === 'select' && (
+                        <select
+                            {...register(field.name, {
+                                required: field.required ? 'Este campo es obligatorio' : false,
+                                validate: field.validate || {},
+                            })}
+                            className="custom-form__select"
+                            name={field.name}
+                            defaultValue={field.defaultValue || ''}
+                            disabled={field.disabled}
+                            onChange={field.onChange}
+                        >
+                            <option value="">Seleccionar opción</option>
+                            {field.options && field.options.map((option, optIndex) => (
+                                <option className="custom-form__option" key={optIndex} value={option.value}>
+                                    {option.label}
+                                </option>
+                            ))}
+                        </select>
+                    )}
+                    {field.type === 'password' && field.name === 'password' && (
+                        <span className="custom-form__toggle-password" onClick={togglePasswordVisibility}>
+                            <img src={showPassword ? ViewIcon : HideIcon} alt="Toggle password visibility" />
+                        </span>
+                    )}
+                    {field.type === 'password' && field.name === 'newPassword' && (
+                        <span className="custom-form__toggle-password" onClick={toggleNewPasswordVisibility}>
+                            <img src={showNewPassword ? ViewIcon : HideIcon} alt="Toggle password visibility" />
+                        </span>
+                    )}
+                    <div className={`custom-form__error ${errors[field.name] || field.errorMessageData ? 'custom-form__error--visible' : ''}`}>
+                        {errors[field.name]?.message || field.errorMessageData || ''}
+                    </div>
+                </div>
+            ))}
+            {buttonText && <button className="custom-form__submit" type="submit">{buttonText}</button>}
+            {footerContent && <div className="custom-form__footer">{footerContent}</div>}
+        </form>
+    );
+};
+
+export default CursoFormPopup;
