14662217764e7519d1bdb57d3e5181f04e89e9a0
HallamSaaveda <hallam.saavedra2001@alumnos.ubiobio.cl>
2025-01-05 21:36:07 -0300

    quito el env

--- frontend/src/services/news.service.js
+++ frontend/src/services/news.service.js
@@ -1,6 +1,6 @@
 import axios from '@services/root.service.js';
 
-const API_URL = import.meta.env.VITE_API_URL || '146.83.198.35:1348';
+const API_URL = '146.83.198.35:1348';
 
 export const createNews = async (newsData) => {
   try {
